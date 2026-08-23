589ac6675dce81a48bd6135cb6a5dd0acdf6cd52
Phan Tuan Duong <duongpt4307@gmail.com>
2026-08-23 22:47:47 +0700

    Update mart_user_os.sql

--- dbt/models/gold/mart_user_os.sql
+++ dbt/models/gold/mart_user_os.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
 SELECT
-    JSON_VALUE(event_data, '$.os_name') AS os_name,
+    JSON_VALUE(device, '$.os_name') AS os_name,
     session_id
 FROM {{ ref('silver_user_event_tracking') }} AS scd
 GROUP BY
